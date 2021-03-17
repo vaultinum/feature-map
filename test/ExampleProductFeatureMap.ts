d6c8d0bc5cba8ecd52eafbe5970c2da8e1ffc76c
Guillaume Acard <guillaume.acard@gmail.com>
2021-03-17 18:12:15 +0100

    Added generated code for build to pass

---
+++ test/ExampleProductFeatureMap.ts
@@ -0,0 +1,41 @@
+export const ExampleProductFeatureMap = {
+    Authentication: {
+        $name: "Authentication",
+        LoginPage: {
+            $name: "Login page",
+            Login: {
+                $name: "Login"
+            },
+            RememberMe: {
+                $name: "Remember me"
+            },
+            ForgotPassword: {
+                $name: "Forgot password"
+            }
+        },
+        RegisterPage: {
+            $name: "Register page",
+            Register: {
+                $name: "Register"
+            },
+            TermsConditions: {
+                $name: "Terms & conditions"
+            }
+        }
+    },
+    MainApplication: {
+        $name: "Main application",
+        Logout: {
+            $name: "Logout"
+        },
+        AccountManagement: {
+            $name: "Account management",
+            AccountSettings: {
+                $name: "Account settings"
+            },
+            UserProfile: {
+                $name: "User profile"
+            }
+        }
+    }
+}
